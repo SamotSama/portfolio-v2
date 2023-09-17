59482804d1373b6641d51f8f49d66306b1443242
Alejandro Rosa <halejandrorosa@gmail.com>
2023-09-16 23:31:51 -0300

    Update LatestCode.js

--- components/LatestCode.js
+++ components/LatestCode.js
@@ -9,8 +9,8 @@ export default function LatestCode({ repositories }) {
   return (
     <section className="bg-[#F1F1F1] -mt-40 dark:bg-gray-900 pb-40">
       <div className="max-w-6xl mx-auto">
-        <div className="flex flex-col md:flex-row justify-between items-center md:pt-40 mx-10">
-          <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center lg:text-left">
+        <div className="flex flex-col md:flex-row justify-between items-center md:pt-20 mx-10">
+          <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 mt-48 mb-11 md:my-20 md:text-white dark:text-gray-600 text-center lg:text-left">
             Latest Code
           </h1>
 
